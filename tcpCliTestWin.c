19e439ad2e09b12276c04ff5aa4c2b659bb803ed
unknown <dennis_otu@live.com>
2026-04-19 21:24:33 +0000

    First commit

---
+++ tcpCliTestWin.c
@@ -0,0 +1,92 @@
+//=---------------------------------------------------------------------------=
+//=   Build:                                                                  =
+//=            MinGW: gcc tcpCliTestWin.c -lws2_32 -o tcpCliTestWin           =
+//=            Borland: bcc32 tcpServer.c                                     =
+//=            Visual C: cl tcpServer.c wsock32.lib                           =
+//=---------------------------------------------------------------------------=
+
+
+
+#include <stdio.h>          // Needed for printf()
+#include <string.h>         // Needed for memcpy() and strcpy()
+#include <stdlib.h>         // Needed for exit()
+#include <windows.h>      // Needed for all Winsock stuff
+#include <stdbool.h>
+
+#define  PORT_NUM         2000   // Port number used at the server
+#define  IP_ADDR    "127.0.0.1"  // IP address of server (*** HARDWIRED ***)
+
+int main(){
+  WORD wVersionRequested = MAKEWORD(1,1);       // For WSA functions
+  WSADATA wsaData;                              // For WSA functions
+  int                  sockfd;        
+  struct sockaddr_in   serv_addr;     
+  char                 out_buf[4096];   
+  char                 in_buf[4096];  
+  int                  ret;        
+  // Initialize winsock
+  WSAStartup(wVersionRequested, &wsaData);
+  // Create a client socket
+  //   - AF_INET is Address Family Internet and SOCK_STREAM is streams
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0){
+    printf("*** ERROR - socket() failed \n");
+    exit(-1);
+  }
+
+  serv_addr.sin_family = AF_INET;                 
+  serv_addr.sin_port = htons(PORT_NUM);           
+  serv_addr.sin_addr.s_addr = inet_addr(IP_ADDR); 
+
+  ret = connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+  if (ret < 0){
+    printf("*** ERROR - connect() failed \n");
+    exit(-1);
+  }
+
+  memset(in_buf, 0, 4096);
+  memset(out_buf, 0, 4096);
+  
+  while (true){
+    printf("Enter message (type 'exit' to close): ");
+    fgets(in_buf, 4096, stdin);
+    // Remove new line character if present
+    in_buf[strcspn(in_buf, "\n")] = '\0';
+
+    if (strcmp(in_buf, "exit") == 0) {
+      break;
+    }
+    // Send to the server using the client socket
+    strcpy(out_buf, in_buf);
+
+    ret = send(sockfd, out_buf, (strlen(out_buf) + 1), 0);
+    if (ret < 0){
+      printf("*** ERROR - send() failed \n");
+      exit(-1);
+    }
+    
+    memset(in_buf, 0 , 4096);
+    memset(out_buf, 0, 4096);
+    // Receive from the server using the client socket
+    ret = recv(sockfd, in_buf, sizeof(in_buf), 0);
+    if (ret < 0){
+      printf("*** ERROR - recv() failed \n");
+      exit(-1);
+    }
+    // Output the received message
+    printf("Server Response: %s \n", in_buf);
+  }
+  // Close the client socket
+  ret = closesocket(sockfd);
+  if (ret < 0){
+    printf("*** ERROR - closesocket() failed \n");
+    exit(-1);
+  }
+  // Clean-up winsock
+  WSACleanup();
+  // Return zero and terminate
+  return(0);
+}
+
+
---
+++ tcpServTestWin.c
@@ -0,0 +1,116 @@
+//=---------------------------------------------------------------------------=
+//=   Build:                                                                  =
+//=            MinGW: gcc tcpServTestWin.c -lws2_32 -o tcpServTestWin         =
+//=            Borland: bcc32 tcpServer.c                                     =
+//=            Visual C: cl tcpServer.c wsock32.lib                           =
+//=---------------------------------------------------------------------------=
+
+
+
+#include <stdio.h>          // Needed for printf()
+#include <string.h>         // Needed for memcpy() and strcpy()
+#include <stdlib.h>         // Needed for exit()
+#include <windows.h>      // Needed for all Winsock stuff
+#include <stdbool.h>    
+
+#define  PORT_NUM   2000    // Arbitrary port number for the server
+
+int main(){
+  WORD wVersionRequested = MAKEWORD(1,1);   // For WSA functions
+  WSADATA wsaData;                          // For WSA functions              
+  int                  sockfd;       
+  struct sockaddr_in   serv_addr;     
+  int                  cli_sockfd;       
+  struct sockaddr_in   cli_addr;     
+  struct in_addr       cli_ip;  
+  int                  addr_len;        
+  char                 out_buf[4096];   
+  char                 in_buf[4096];    
+  int                  ret;         
+  // Iitialize winsock
+  WSAStartup(wVersionRequested, &wsaData);
+  // Create a welcome socket
+  //   - AF_INET is Address Family Internet and SOCK_STREAM is streams
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0)
+  {
+    printf("*** ERROR - socket() failed \n");
+    exit(-1);
+  }
+
+  serv_addr.sin_family = AF_INET;                 
+  serv_addr.sin_port = htons(PORT_NUM);           
+  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); 
+
+  ret = bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    
+  if (ret < 0){
+    printf("*** ERROR - bind() failed \n");
+    exit(-1);
+  }
+  // Listen on welcome socket for a connection
+  listen(sockfd, 5);
+  // Accept a connection.  The accept() will block and then return with
+  // cli_sockfd assigned and cli_addr filled-in.
+  printf("Server Started: Listening... \n");
+
+  addr_len = sizeof(cli_addr);
+  cli_sockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &addr_len);
+
+  if (cli_sockfd < 0){
+    printf("*** ERROR - accept() failed \n");
+    exit(-1);
+  }
+  // Copy the four-byte client IP address into an IP address structure
+  memcpy(&cli_ip, &cli_addr.sin_addr.s_addr, 4);
+
+  // Print an informational message that accept completed
+  printf("Client Connected: IP Address = %s  Port = %d \n",
+    inet_ntoa(cli_ip), ntohs(cli_addr.sin_port));
+
+  memset(in_buf, 0, 4096);
+  memset(out_buf, 0, 4096);
+
+  while (true){
+    // Receive from the client using the connect socket
+    ret = recv(cli_sockfd, in_buf, sizeof(in_buf), 0);
+
+    if (ret < 0){
+      printf("Client Disconnected: Server Closed \n");
+      exit(-1);
+    }
+
+    printf("Client Message: %s \n", in_buf);
+
+    // Send to the client using the connect socket
+    strcpy(out_buf, "message recieved");
+    ret = send(cli_sockfd, out_buf, (strlen(out_buf) + 1), 0);
+
+    if (ret < 0){
+      printf("*** ERROR - failed to send response \n");
+      exit(-1);
+    }
+
+    memset(in_buf, 0, 4096);
+    memset(out_buf, 0, 4096);
+  }
+  // Close the sockets
+  ret = closesocket(sockfd);
+  if (ret < 0)
+  {
+    printf("*** ERROR - closesocket() failed \n");
+    exit(-1);
+  }
+  
+  ret = closesocket(cli_sockfd);
+  if (ret < 0)
+  {
+    printf("*** ERROR - closesocket() failed \n");
+    exit(-1);
+  }
+  // Clean-up winsock
+  WSACleanup();
+  // Return zero and terminate
+  return(0);
+}
+
